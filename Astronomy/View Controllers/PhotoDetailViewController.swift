8825be34db2274aef78520ae7790c2d3d8d74df7
Nicolas Rios <nickrios33@yahoo.com>
2020-03-28 00:47:21 -0700

    Add files via upload

---
+++ Astronomy/View Controllers/PhotoDetailViewController.swift
@@ -0,0 +1,86 @@
+//
+//  PhotoDetailViewController.swift
+//  Astronomy
+//
+//  Created by Andrew R Madsen on 9/9/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class PhotoDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViews()
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        guard let image = imageView.image else { return }
+        
+        PHPhotoLibrary.requestAuthorization{ (status) in
+            switch status {
+            case.authorized:
+                PHPhotoLibrary.shared().performChanges({
+                    PHAssetChangeRequest.creationRequestForAsset(from: image)
+                }, completionHandler: { (success, error) in
+                    if let error = error {
+                        NSLog("Error saving photo: \(error)")
+                        return
+                    }
+                })
+            default:
+                DispatchQueue.main.async{
+                    let alert = UIAlertController(title:"Could not save image", message:"Astronomy does not have access to your Photo libarary.Please change this in Settings if you would like to save images.",preferredStyle:.alert)
+                    
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler:nil))
+                    
+                    self.present(alert,animated: true,completion: nil)
+                }
+                break
+            }
+        }
+        
+    }
+    
+    // MARK: - Private
+    
+    private func updateViews() {
+        guard let photo = photo, isViewLoaded else { return }
+        
+        
+        let dateString = dateFormatter.string(from: photo.earthDate)
+        detailLabel.text = "Taken by \(photo.camera.roverId) on \(dateString) (Sol \(photo.sol))"
+        cameraLabel.text = photo.camera.fullName
+        title = dateString
+        
+        do {
+            let data = try Data(contentsOf: photo.imageURL.usingHTTPS!)
+            imageView.image = UIImage(data: data)
+            
+        } catch {
+            NSLog("Error setting up views on detail view controller: \(error)")
+        }
+    }
+    
+    // MARK: - Properties
+    
+    var photo: MarsPhotoReference? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    lazy var dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateStyle = .short
+        df.timeStyle = .short
+        return df
+    }()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var cameraLabel: UILabel!
+    
+}
